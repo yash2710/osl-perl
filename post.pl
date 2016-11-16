70a7713e4bef007f6da706f04d8ae3174788c703
yzthakker <yzthakker@gmail.com>
2016-11-16 23:49:24 +0530

    Update post.pl

--- post.pl
+++ post.pl
@@ -1,9 +1,9 @@
 use Firebase;
 my $fb=Firebase->new(firebase => 'chat-perl',
 auth => {secret => 'rrvWXn5AYHgFBjb6REDQ6r3v3IxjMLa4E8mh9kES',
-data => {uid => 'xxx', username => 'fred'}, admin => \1
+data => {uid => 'xxx', username => 'yash'}, admin => \1
 });
-print('Enter your Email'."\n");
+print('Enter your mail ID'."\n");
 $email = <STDIN>;
 chomp($email);
 print('Enter your Message'."\n");
